659940e3286f79482c76cb4fb1e3e6adceb46d39
ITITIU19087 <69331679+ITITIU19087@users.noreply.github.com>
2023-10-14 14:43:21 +0700

    OrderDTO and create order service

---
+++ src/main/java/com/ordermatching/service/hazelcast/OrderService.java
@@ -0,0 +1,30 @@
+package com.ordermatching.service.hazelcast;
+
+import com.hazelcast.map.IMap;
+import com.ordermatching.config.HazelcastConfig;
+import com.ordermatching.dto.OrderDto;
+import com.ordermatching.entity.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+    @Autowired
+    private HazelcastConfig hazelcastConfig;
+
+    public void createOrder(OrderDto orderDto){
+        IMap<String, Order> orderMap = hazelcastConfig.hazelcastInstance().getMap("orders");
+        Order order = new Order();
+        String orderId = UUID.randomUUID().toString();
+        order.setUUID(orderId);
+        order.setPrice(orderDto.getPrice());
+        order.setQuantity(orderDto.getQuantity());
+        order.setSide(orderDto.getSide());
+        order.setOrderTime(LocalDateTime.now());
+        order.setStatus("Success");
+        orderMap.put(order.getUUID(), order);
+    }
+}

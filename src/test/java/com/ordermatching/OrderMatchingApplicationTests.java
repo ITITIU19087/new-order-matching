b216ca53e7406da2fb830877ace4cc935f47b9b0
ITITIU19087 <69331679+ITITIU19087@users.noreply.github.com>
2023-10-14 14:24:51 +0700

    init

---
+++ src/test/java/com/ordermatching/OrderMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordermatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMatchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6e30705eeec803eebf3cd69b9f9c42553a1328b7
ITITIU19087 <69331679+ITITIU19087@users.noreply.github.com>
2023-11-28 16:15:16 +0700

    update time interval attr for TradePrice

--- src/main/java/com/ordermatching/entity/TradePrice.java
+++ src/main/java/com/ordermatching/entity/TradePrice.java
@@ -6,13 +6,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class TradePrice {
-    public TradePrice(Double openPrice, Double closePrice, Double maxPrice, Double minPrice) {
+    public TradePrice(String interval, Double openPrice, Double closePrice, Double maxPrice, Double minPrice) {
+        this.interval = interval;
         this.openPrice = openPrice;
         this.closePrice = closePrice;
         this.maxPrice = maxPrice;
         this.minPrice = minPrice;
     }
 
+    private String interval;
     private Double openPrice;
     private Double closePrice;
     private Double maxPrice;

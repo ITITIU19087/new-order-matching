40254cdd8241891b00cf4d398ef4838af03510be
ITITIU19087 <69331679+ITITIU19087@users.noreply.github.com>
2023-12-10 21:05:34 +0700

    Jet Instance

--- src/main/java/com/ordermatching/config/HazelcastConfig.java
+++ src/main/java/com/ordermatching/config/HazelcastConfig.java
@@ -3,6 +3,9 @@ package com.ordermatching.config;
 import com.hazelcast.config.*;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.config.JetConfig;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +18,15 @@ public class HazelcastConfig {
         config.setInstanceName("orders");
         JoinConfig joinConfig = config.getNetworkConfig().getJoin();
         joinConfig.getMulticastConfig().setEnabled(true);
-        config.setClusterName("order-matching");
         config.getJetConfig().setEnabled(true);
+        config.setClusterName("order-matching");
         return config;
+    }
+
+    @Bean
+    public JetInstance jetInstance(){
+        JetConfig config = hazelcastConfig().getJetConfig();
+        return Jet.newJetInstance(config);
     }
 
     @Bean

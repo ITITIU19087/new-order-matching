132aa0d29e64bfcc694d9ce71ff15d6c550ea701
ITITIU19087 <69331679+ITITIU19087@users.noreply.github.com>
2024-01-16 19:11:21 +0700

    Socket IO config

--- src/main/java/com/ordermatching/config/WebSocketConfig.java
+++ src/main/java/com/ordermatching/config/WebSocketConfig.java
@@ -1,22 +1,32 @@
 package com.ordermatching.config;
 
 import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 
 @org.springframework.context.annotation.Configuration
-public class WebSocketConfig {
-    @Bean(destroyMethod = "stop")
+public class WebSocketConfig{
+    @Bean
     public SocketIOServer socketIOServer(){
         Configuration config = new Configuration();
         config.setHostname("localhost");
         config.setPort(9999);
+        config.setOrigin("http://localhost:3000"); // Allows connections from any origin
 
-        SocketIOServer server =  new SocketIOServer(config);
+        final SocketIOServer server = new SocketIOServer(config);
+
+        server.addConnectListener(new ConnectListener() {
+            @Override
+            public void onConnect(SocketIOClient client) {
+
+            }
+        });
 
         server.start();
-
         return server;
     }
 }
--- src/main/java/com/ordermatching/service/socketio/SocketIOService.java
+++ src/main/java/com/ordermatching/service/socketio/SocketIOService.java
@@ -1,23 +1,26 @@
 package com.ordermatching.service.socketio;
 
 import com.corundumstudio.socketio.SocketIOServer;
-import com.ordermatching.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Map;
 
 @Service
 public class SocketIOService {
     private SocketIOServer server;
-
 
     @Autowired
     public SocketIOService(SocketIOServer server) {
         this.server = server;
     }
 
-    public void notifyOrderCreation(String message) {
-        server.getBroadcastOperations().sendEvent("orderCreated", message);
+    public void notifyOrderCreation(Map<Double, Long> msg1, Map<Double, Long> msg2) {
+        server.getBroadcastOperations().sendEvent("buyEvent", msg1);
+        server.getBroadcastOperations().sendEvent("sellEvent",msg2);
+    }
+
+    public void dumps(String msg) {
+        server.getBroadcastOperations().sendEvent("buyEvent", msg);
     }
 }
